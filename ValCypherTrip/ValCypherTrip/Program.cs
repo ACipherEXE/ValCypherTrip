c69da19c7bd385d4f7ecdf0d8e8ceadd2b7b66c9
Christopher Negron <sonycar2000@outlook.com>
2020-10-06 19:20:18 -0400

    Add files via upload

---
+++ ValCypherTrip/ValCypherTrip/Program.cs
@@ -0,0 +1,39 @@
+﻿/*Chirstopher J Negron
+ * 10/6/2020
+ * 
+ * I have been playing Valorant for a while and I wanted to make something fast for the agent I play
+ * named Cypher. Cypher is a control character that can lock down areas and one of his tools is the tripwire, 
+ * its used to give informacion to the team about where others are entering. 
+ * The point of this project is to do the same thing.
+ * 
+ * 
+ * 
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValCypherTrip
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tripwire test = new Tripwire();
+            String tripName = "Trip 1"; //Enter the name of the trip
+            String tripOutput = "set";// enter the output
+            //Types of output
+            /*
+             * alarm: Tripwire activated
+             * set: Trip has been set
+             * error: something went worng
+             * 
+             * But if nothing:
+             * it will return null
+             */
+            Console.WriteLine(test.soundTrigger(tripOutput, tripName));
+        }
+    }
+}
---
+++ ValCypherTrip/ValCypherTrip/Tripwire.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Media;
+
+namespace ValCypherTrip
+{
+    class Tripwire
+    {
+        public String soundTrigger(String input, String tripNumber)
+        {
+
+            if ("alarm" == input || "Alarm" == input) //Tripwire activated
+            {
+                Console.WriteLine("Alarmed Triggered ");
+                int n = 10;
+                for (int i = 1; i < n; i++)
+                    Console.Beep();
+     
+                return "Message From: " + tripNumber;
+            }
+            if ("set" == input || "Set" == input) //Tripwire Set
+            {
+                Console.WriteLine("Alarm Set");
+                int n = 2;
+                for (int i = 1; i < n; i++)
+                    Console.Beep();
+
+                return "Message From: " + tripNumber;
+            }
+            if ("error" == input || "Error" == input) //Something went worng
+            {
+                Console.WriteLine("Alarm Error");
+                int n = 5;
+                for (int i = 1; i < n; i++)
+                    Console.Beep();
+
+                return "Message From: " + tripNumber;
+            }
+            else //If all fails
+            {
+                return null;
+            }
+            
+            
+        }
+    }
+}
